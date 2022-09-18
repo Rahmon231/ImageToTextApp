a087e745b2b03b9306a71a20f28c43251d4ccd18
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-18 01:50:54 +0100

    activity_scanner ui

--- app/src/main/java/com/lemzeeyyy/imagetotextapp/ScannnerActivity.java
+++ app/src/main/java/com/lemzeeyyy/imagetotextapp/ScannnerActivity.java
@@ -3,15 +3,12 @@ package com.lemzeeyyy.imagetotextapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private Button startBtn;
+public class ScannnerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startBtn = findViewById(R.id.start_btn);
+        setContentView(R.layout.activity_scannner);
     }
 }
